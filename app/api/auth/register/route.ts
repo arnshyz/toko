f36a65928e6efa87d73f29d629a5daf2e74a5f24
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-10-04 10:01:20 +0700

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -8,13 +8,19 @@ export async function POST(req: NextRequest) {
   const name = String(form.get('name') || '');
   const email = String(form.get('email') || '').toLowerCase();
   const password = String(form.get('password') || '');
-  if (!name || !email || !password) return NextResponse.json({ error: 'Invalid' }, { status: 400 });
-
+  if (!name || !email || !password) {
+    return NextResponse.json({ error: 'Invalid' }, { status: 400 });
+  }
   const exists = await prisma.user.findUnique({ where: { email } });
   if (exists) return NextResponse.json({ error: 'Email sudah terdaftar' }, { status: 400 });
 
   const passwordHash = await bcrypt.hash(password, 10);
-  const slug = slugify(name);
-  await prisma.user.create({ data: { name, email, passwordHash, slug, isAdmin: false } });
+  await prisma.user.create({ data: { name, email, passwordHash, slug: slugify(name), isAdmin: false } });
+
   return NextResponse.redirect(new URL('/seller/login', req.url));
+}
+
+
+export function GET(req: NextRequest) {
+  return NextResponse.redirect(new URL('/seller/register', req.url));
 }
