f48475fc5b55b4b555ba5db345ff2470a3b8a88d
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-10-04 09:44:35 +0700

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -1,7 +1,15 @@
-import { IronSessionOptions } from "iron-session";
-export const sessionOptions: IronSessionOptions = {
+import type { SessionOptions } from "iron-session";
+
+export const sessionOptions: SessionOptions = {
   password: process.env.IRON_SESSION_PASSWORD!,
   cookieName: "akay_session",
   cookieOptions: { secure: process.env.NODE_ENV === "production" },
 };
-export type SessionUser = { id: string; name: string; email: string; slug: string; isAdmin: boolean; };
+
+export type SessionUser = {
+  id: string;
+  name: string;
+  email: string;
+  slug: string;
+  isAdmin: boolean;
+};
