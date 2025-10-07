94b28d367c226e4ffbb869157a2eea9db12b9439
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-10-08 00:23:12 +0700

    Add migration for user store address fields

---
+++ prisma/migrations/20251004000017_add_user_store_address_fields/migration.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "User"
+  ADD COLUMN "storeAddressLine" TEXT,
+  ADD COLUMN "storeProvince" TEXT,
+  ADD COLUMN "storeCity" TEXT,
+  ADD COLUMN "storeDistrict" TEXT,
+  ADD COLUMN "storePostalCode" TEXT,
+  ADD COLUMN "storeOriginCityId" TEXT;
